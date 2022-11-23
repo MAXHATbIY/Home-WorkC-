8860919819adf10288a997dff604d513c1269e14
Slava <mc_slava2@mail.ru>
2022-11-23 21:26:01 +0300

    add EX301302

---
+++ Example301/Program.cs
@@ -0,0 +1,32 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.Clear();
+int num1 = GetNumberFromUser("Введите целое число: ", "Ошибка ввода!");
+int num2 = GetNumberFromUser("Введите второе целое число: ", "Ошибка ввода!");
+int result = GetNaturalPowerNumbers(num1, num2);
+Console.WriteLine($"{num1}, {num2} -> {result}");
+
+///////////////////////
+
+int GetNaturalPowerNumbers(int num1, int num2)
+{
+    int product = 1;
+    for(int i = 0; i < num2; i++)
+    product *= num1;
+    return product;
+}
+
+///////////////////////////////////////////////
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while(true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if(isCorrect)
+            return userNumber;
+        else 
+            Console.WriteLine(errorMessage);
+    }
+}
---
+++ Example302/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+
+Console.Clear();
+int num = GetNumberFromUser("Введите целое число: ", "Ошибка ввода!");
+int digitsSum = GetDigitsSum(num);
+Console.WriteLine($"{num} -> {digitsSum}");
+
+/////////////////////////////////
+
+int GetDigitsSum(int num)
+{
+    int total = 0;
+    do
+    {
+        total += num % 10;
+        num /= 10;
+    }
+    while(num != 0);
+    return total;
+}
+
+
+
+
+
+///////////////////////////////
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while(true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if(isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    } 
+}
