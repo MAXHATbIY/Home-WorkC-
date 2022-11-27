a7b104c479454963575dd48fff333c4632b7daa8
Slava <mc_slava2@mail.ru>
2022-11-27 09:23:08 +0300

    add Ex402

--- Example402/Program.cs
+++ Example402/Program.cs
@@ -3,16 +3,19 @@
 
 Console.Clear();
 
+// заполнение массива
 int[] array = GetArray(15, 1, 10);
 Console.Write("[ ");
 Console.Write(String.Join(" ", array));
 Console.Write(" ]");
 
 
+// переменная для подсчета элементов
+int sum = GetSumOddPositions(array);
+Console.WriteLine($" сумма элементов стоящих на нечетных позициях  -> {sum}");
 
 
-
-
+// генерируем массив
 int[] GetArray(int size, int minValue, int maxValue)
 {
     int[] res = new int[size];
@@ -23,13 +26,13 @@ int[] GetArray(int size, int minValue, int maxValue)
     return res;
 }
 
-
-
-
-
-
-
-
-
-
-
+// считаем элементы стоящие на нечетных позициях
+int GetSumOddPositions(int[] array)
+{
+    int sum = 0;
+    for(int i = 1; i < array.Length; i += 2)
+    {
+        sum += array[i];
+    }
+    return sum;
+}
