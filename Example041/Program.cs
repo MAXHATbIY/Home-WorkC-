e39e64e21add7880e22a937fc229ab44809a88d1
Slava <mc_slava2@mail.ru>
2022-11-30 18:00:29 +0300

    add Ex041

---
+++ Example041/Program.cs
@@ -0,0 +1,36 @@
+﻿
+// Задача 41: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int num = GetNumberFromUser("Введите длинну массива: ", "Ошибка ввода!");
+int posNum = CountPositiveNumbers(num);
+Console.WriteLine($"Количество положительных чисел -> {posNum}");
+
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while(true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if(isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+int CountPositiveNumbers(int num)
+{
+    int count = 0;
+    int[] array = new int[num];
+    for (int i = 0; i < num; i++)
+    {
+        array[i] = GetNumberFromUser("Введите число: ", "Ошибка Ввода!");
+        if(array[i] > 0)
+        count++;
+    }
+    Console.Write("[ ");
+    Console.Write(string.Join(" ", array));
+    Console.Write(" ] ");
+    return count;
+}
