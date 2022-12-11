acad5374c28843345ce362b4d1726b406b0d543a
Slava <mc_slava2@mail.ru>
2022-12-11 21:34:29 +0300

    Added 64-68

---
+++ Example064/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+int start = UserInput("Введите целое число  N: ", "Ошибка!");
+
+string result  = Numbers(start, 1);
+
+Console.WriteLine(result);
+
+//==============Методы
+ //Ввод данных
+int UserInput(string message, string errorMessage)
+{    
+     while(true)
+    {
+        Console.Write(message);
+        if( (int.TryParse(Console.ReadLine(), out int userNumber))&& userNumber>0)
+
+            return userNumber;
+        Console.WriteLine(errorMessage);
+     } 
+}
+
+//Вывод чисел рекурсией
+string Numbers(int start, int finish)
+{
+    if (start==finish)
+    return start.ToString();
+
+    return start + " " + Numbers(start - 1, finish);
+
+}
---
+++ Example066/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 66: Задайте значения M и N.
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.Clear();
+
+int m = GetNumberFromUser("Введите первое целое положительное число:  ","Ошибка ввода!");
+int n = GetNumberFromUser("Введите второе целое положительное число:  ","Ошибка ввода!");
+
+int numSum = FindNumbersSum(m, n);
+
+Console.WriteLine($"M = {m}, N = {n} -> {numSum}");
+
+// метод для поиска суммы натуральных чисел на промежутке от M до N
+int FindNumbersSum(int num1, int num2)
+{
+    if(num1 == num2) return num1;
+    else return num1 + FindNumbersSum(num1 + 1, num2);
+}
+
+
+//  метод получения числа от пользователя
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while(true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if(isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    } 
+}
+
